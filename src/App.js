b75fb4f96d9bf1f8242d004aa5c85d6c081724fe
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-08-27 11:01:19 +0200

    files uploaded

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-elastic-carousel';
+import MovieCard from './components/MovieCard';
+const App = () => {
+	let breakPoints = [
+		{ width: 500, itemsToShow: 1 },
+		{ width: 700, itemsToShow: 2 },
+		{ width: 1200, itemsToShow: 4 },
+		{ width: 1500, itemsToShow: 6 },
+	];
+	return (
+		<div className='App mt-4'>
+			<Carousel breakPoints={breakPoints}>
+				<MovieCard />
+				<MovieCard />
+				<MovieCard />
+				<MovieCard />
+				<MovieCard />
+				<MovieCard />
+				<MovieCard />
+				<MovieCard />
+			</Carousel>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => {
+	return (
+		<div className='card'>
+			<img
+				src='https://m.media-amazon.com/images/M/MV5BNzU4NWEwNDItMzMzYy00ZDYyLWIxZjMtMDlkYWVjNjQwYzBjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg'
+				alt=''
+			/>
+		</div>
+	);
+};
+
+export default Card;
