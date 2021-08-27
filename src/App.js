d7acac5ed55513e04e4d2f797844bb9227072dfd
Azizbek Tokhirjonov <azizwebdev77@gmail.com>
2021-08-27 14:45:33 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Gallery1 from './components/Gallery1';
-import Gallery2 from './components/Gallery2';
-import Gallery3 from './components/Gallery3';
-import SearchQeury from './components/SearchQeury';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Gallery1 from "./components/Gallery1";
+import Gallery2 from "./components/Gallery2";
+import Gallery3 from "./components/Gallery3";
+import SearchQeury from "./components/SearchQeury";
+import NavBar from "./components/Navbar";
 
 const App = () => {
-	return (
-		<div className='App mt-4'>
-			<SearchQeury />
-			<Gallery1 />
-			<Gallery2 />
-			<Gallery3 />
-		</div>
-	);
+  return (
+    <div className="App">
+      <NavBar />
+      <SearchQeury />
+      <Gallery1 />
+      <Gallery2 />
+      <Gallery3 />
+    </div>
+  );
 };
 export default App;
