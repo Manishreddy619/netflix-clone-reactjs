dd00e0b3870283c00de500ac1ad1d4884c97f7f2
Azizbek Tokhirjonov <azizwebdev77@gmail.com>
2021-08-27 16:16:20 +0200

    fixed issues and added search functionality lined to navbar. added styles

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <SearchQeury />
       <Gallery1 />
       <Gallery2 />
       <Gallery3 />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,53 +8,64 @@ import {
   Dropdown,
 } from "react-bootstrap";
 import { Search, ArrowRight } from "react-bootstrap-icons";
-const NavBar = () => {
-  return (
-    <Navbar expand="lg">
-      <Navbar.Brand href="#home">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png"
-          alt=""
-          style={{ width: 100 }}
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">TV Shows</Nav.Link>
-          <Nav.Link href="#link">Movies</Nav.Link>
-          <Nav.Link href="#link">Recently Added</Nav.Link>
-          <Nav.Link href="#link">My List</Nav.Link>
-        </Nav>
-        <Nav className="ml-auto">
-          <Nav.Link href="#deets">
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="mr-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">
-                <Search color="white" size={24} />
-              </Button>
-            </Form>
-          </Nav.Link>
-          <div className="mt-2">
-            <SplitButton
-              align={{ lg: "start" }}
-              title="USER"
-              id="dropdown-menu-align-responsive-2"
-            >
-              <Dropdown.Item eventKey="1">Action 1</Dropdown.Item>
-              <Dropdown.Item eventKey="2">Action 2</Dropdown.Item>
-            </SplitButton>
-          </div>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
+import SearchQeury from "./SearchQeury";
+import { Component } from "react";
+import "./Navbar.css";
+class NavBar extends Component {
+  state = {
+    buttonClick: false,
+  };
+  render() {
+    return (
+      <>
+        <Navbar expand="lg" className="nav-change">
+          <Navbar.Brand href="#home">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png"
+              alt=""
+              style={{ width: 100 }}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">TV Shows</Nav.Link>
+              <Nav.Link href="#link">Movies</Nav.Link>
+              <Nav.Link href="#link">Recently Added</Nav.Link>
+              <Nav.Link href="#link">My List</Nav.Link>
+            </Nav>
+            <Nav className="ml-auto">
+              <Nav.Link href="#deets">
+                <Form className="d-flex">
+                  <Button
+                    onClick={(e) =>
+                      this.setState({ buttonClick: !this.state.buttonClick })
+                    }
+                    variant="outline-success"
+                  >
+                    <Search color="white" size={24} />
+                  </Button>
+                </Form>
+                {/* <SearchQeury /> */}
+              </Nav.Link>
+              <div className="mt-2">
+                <SplitButton
+                  align={{ lg: "start" }}
+                  title="USER"
+                  id="dropdown-menu-align-responsive-2"
+                >
+                  <Dropdown.Item eventKey="1">Action 1</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">Action 2</Dropdown.Item>
+                </SplitButton>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        {this.state.buttonClick && <SearchQeury />}
+      </>
+    );
+  }
+}
 
 export default NavBar;
